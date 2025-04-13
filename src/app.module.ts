7c86040a606aea8a00431126f4aac801befc2237
Eugene Chekan <hom3chuk@gmail.com>
2025-04-13 17:13:28 +0200

    add prisma/token

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GoogleServerOauthModule } from './google-server-oauth/google-server-oauth.module'
 import { ConfigModule } from '@nestjs/config'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   imports: [ConfigModule.forRoot(), GoogleServerOauthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
